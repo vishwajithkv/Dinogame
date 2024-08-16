957f7c638f0cd5934c50fbed6d0a4c32adc3ad24
vishwajithkv <leoxsyspriv@gmail.com>
2024-08-16 15:12:56 +0000

    Create a header for constants

--- dino.cpp
+++ dino.cpp
@@ -3,26 +3,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include "headers/constants.h"
 
-// Define some constants
-#define SCREEN_WIDTH 800   // The width of the screen in pixels
-#define SCREEN_HEIGHT 600  // The height of the screen in pixels
-#define GROUND_HEIGHT 100  // The height of the ground in pixels
-#define DINO_WIDTH 40      // The width of the dino
-#define ObstacleMaxGap 450 // The maximum gap between object in pixels
-#define DINO_HEIGHT 40     // The height of the dino in pixels
-#define OBSTACLE_WIDTH 30  // The width of the obstacle in pixels
-#define OBSTACLE_HEIGHT 30 // The height of the obstacle in pixels
-int obstacleGap = 400;     // The gap between two obstacles in pixels
-#define ObstacleMinGap 150 // The minimum gap between two obstacles in pixels
-#define JUMP_SPEED 40      // The speed of the dino's jump in pixels per frame
-#define GRAVITY 2          // The gravity of the dino in pixels per frame
-int score = 0;
+int obstacleGap = 400;    
+int score = 0;             
 int highscore = 0;
 int bonus = 0;
 
 // Define some colors
-sf::Color GROUND_COLOR = sf::Color(0xa23331ff); // The color of the ground
 sf::Texture obstacleTexture;
 sf::Sprite obstacleSprite;
 
